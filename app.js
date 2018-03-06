c021cdfa071a7012313cf1c19c5b8df1567d29d4
Sreekanth Sundareswaran <ssundare@global.sial.com>
2018-03-06 14:07:18 +0530

    code to avoid CORS errors by changing the headers

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+//this block is to override CORS errors...
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin', '*'); //instead of * we can give the site name also
+  res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if(req.method === 'OPTIONS'){
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();       //to make sure other routes can follow.
+})
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
