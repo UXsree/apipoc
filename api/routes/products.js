eba1388abc26d652963206b603f6886b2f86f743
Sreekanth Sundareswaran <ssundare@global.sial.com>
2018-03-06 13:51:16 +0530

    code to parse the request body using body-parser

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,14 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next)=>{
-  res.status(200).json({
-    message: 'Handling GET request of /products'
+res.status(200).json({
+    message: 'Handling GET request of /products',
   });
 });
 
 router.post('/',(req,res,next)=>{
-  res.status(201).json({
-    message: 'Handling POST request of /products'
+  //capturing the product details using body parser starts here.
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
+   //capturing the product details using body parser ends here.
+    res.status(201).json({
+    message: 'Handling POST request of /products',
+    createdProduct: product //passing the captured body parser product to response json.
   });
 });
 
